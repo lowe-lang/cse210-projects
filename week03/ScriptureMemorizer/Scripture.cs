59a336a6b7fed5fe0ed0b4ec6f3eb6af67537f9b
lowe-lang <lowenjohn46@gmail.com>
2025-09-19 12:41:11 +0100

    Week03

---
+++ week03/ScriptureMemorizer/Scripture.cs
@@ -0,0 +1,32 @@
+class Scripture
+{
+    private Reference _reference;
+    private List<Word> _words;
+
+    public Scripture(Reference reference, string text)
+    {
+        _reference = reference;
+        _words = text.Split(' ').Select(word => new Word(word)).ToList();
+    }
+
+    public void HideRandomWords(int numberToHide)
+    {
+        Random rand = new Random();
+        for (int i = 0; i < numberToHide; i++)
+        {
+            int index = rand.Next(_words.Count);
+            _words[index].Hide();
+        }
+    }
+
+    public string GetDisplayText()
+    {
+        string text = string.Join(" ", _words.Select(w => w.GetDisplayText()));
+        return $"{_reference.GetDisplayText()} {text}";
+    }
+
+    public bool IsCompletelyHidden()
+    {
+        return _words.All(w => w.IsHidden());
+    }
+}
